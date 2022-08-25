1875337ff4269aff4a1e72e2319bca7d1d53675f
facundoivanmonzon <facundoivanmonzon.com>
2022-08-25 20:38:26 -0300

    Groupnueva commitAgregar archivos de proyecto.

---
+++ WebApplication1/controllers/GroupController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using userApp.models;
+
+namespace UserAPP.APi.controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<GroupModel> Groups()
+        {
+            return new GroupModel() { groupId = 1, groupName = "Cantanrte"};
+        }
+    }
+}
