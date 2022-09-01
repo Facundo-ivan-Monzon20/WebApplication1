cfc522c7aca362fe0a92476667dea1c9ed6ffb97
Facundo Monzon <fimonzon@mail.austral.edu.ar>
2022-09-01 14:39:58 -0300

    groups controllers

--- WebApplication1/controllers/GroupController.cs
+++ WebApplication1/controllers/GroupController.cs
@@ -12,7 +12,50 @@ namespace UserAPP.APi.controllers
         [HttpGet]
         public ActionResult<GroupModel> Groups()
         {
-            return new GroupModel() { groupId = 1, groupName = "Cantanrte"};
+            return new GroupModel() { groupId = 1, groupName = "Cantantes"};
+        }
+
+        [HttpGet("{groupId}")]
+        public ActionResult<GroupModel> group([FromRoute] int groupId)
+        {
+            return new GroupModel() {groupId = 3, groupName = "streamers"}
+        }
+
+         // POST USER
+
+        [HttpPost]
+        public ActionResult CreateUser([FromBody] GroupModel group)
+        {
+            return Ok();
+        }
+
+        // PUT USER
+
+        [HttpPut("id")]
+        public ActionResult UpdateUser([FromRoute]int id,[FromBody] GroupModel group)
+        {
+            if (id == -1)
+                return NoContent();
+            return Ok();
+        }
+        // DELETE USER
+        [HttpDelete("id")]
+        public ActionResult deleteUser([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{id}/[Action]", Name = "Activate")]
+        public ActionResult Activate([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+
+        [HttpPatch("{id}/[Action]", Name = "Inactivate")]
+        public ActionResult Inactivate([FromRoute] int id)
+        {
+            return Ok();
         }
     }
 }
